b473fb5d2cf7a0a4cf94dc05240152e8e4935b1c
Adam Jones <adam@linuxtampa.com>
2019-09-22 20:16:00 -0400

    All endpoints for locations work

--- Models/Locations.cs
+++ Models/Locations.cs
@@ -1,3 +1,4 @@
+using System;
 using coincidence.Models;
 using System.Collections.Generic;
 
@@ -9,7 +10,6 @@ namespace coincidence.Models
     public string Address { get; set; }
     public string ManagerName { get; set; }
     public string PhoneNumber { get; set; }
-
     public ICollection<Coincidence> Coins { get; set; }
   }
 }
